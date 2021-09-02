e3cc0328983185bd4996c3c2201dbe3b06a434ce
cham207388 <cham207388@gmail.com>
2021-09-02 15:24:50 -0400

    added custom health indicator.

---
+++ src/main/java/com/immigrant/dream/actuator/CustomHealthIndicator.java
@@ -0,0 +1,20 @@
+package com.immigrant.dream.actuator;
+
+import org.springframework.boot.actuate.health.Health;
+import org.springframework.boot.actuate.health.HealthIndicator;
+import org.springframework.stereotype.Component;
+
+import java.util.Random;
+
+@Component
+public class CustomHealthIndicator implements HealthIndicator {
+	@Override
+	public Health health() {
+		Random random = new Random();
+
+		if (random.nextBoolean()){
+			return Health.down().withDetail("ERR-001", "Random failure").build();
+		}
+		return Health.up().build();
+	}
+}
