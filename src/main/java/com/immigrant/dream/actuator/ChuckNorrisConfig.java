9bb35c63df2d2e64b67fa7ec15992013e91fef56
cham207388 <cham207388@gmail.com>
2021-09-02 15:59:13 -0400

    use chuck norris jar and populate info

---
+++ src/main/java/com/immigrant/dream/actuator/ChuckNorrisConfig.java
@@ -0,0 +1,14 @@
+package com.immigrant.dream.actuator;
+
+import guru.springframework.norris.chuck.ChuckNorrisInfoContributor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ChuckNorrisConfig {
+
+	@Bean
+	public ChuckNorrisInfoContributor chuckNorrisInfoContributor() {
+		return new ChuckNorrisInfoContributor();
+	}
+}
